8187c0aadacbfcc71974679a61c0303746c361d7
srohiit2678 <rohitsikhwal99@gmail.com>
2026-05-12 09:52:23 +0530

    add test case for project object in projectTeamMemberEntity

--- PastExploreHub/src/test/java/com/pastexplorehub/entity/ProjectTeamMemberTest.java
+++ PastExploreHub/src/test/java/com/pastexplorehub/entity/ProjectTeamMemberTest.java
@@ -34,16 +34,30 @@ class ProjectTeamMemberTest {
     }
 
     @Test
+    void testSetAndGetProject() {
+        Project project = new Project();
+
+        projectTeamMember.setProject(project);
+
+        assertEquals(project, projectTeamMember.getProject());
+    }
+
+
+    @Test
     void testTOString(){
+        Project project = new Project();
+
         projectTeamMember.setId(10L);
         projectTeamMember.setName("Rohan");
         projectTeamMember.setEnrollId("0832IT111011");
+        projectTeamMember.setProject(project);
 
         String result  =projectTeamMember.toString();
 
         assertTrue(result.contains("10"));
         assertTrue(result.contains("Rohan"));
         assertTrue(result.contains("0832IT111011"));
+        assertTrue(result.contains("Project"));
     }
 
     @Test
